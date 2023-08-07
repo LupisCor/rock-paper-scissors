4b8b1ce471994a3997a8852153fdc9af009891ff
Lupis_ <doliver575@gmail.com>
2023-08-07 10:31:51 -0400

    Create playround function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,29 @@
 console.log("Hello World");
+
+let rpsArray = ["Rock", "Paper", "Scissors"]
+
+function getComputerChoice(){
+    return rpsArray[(Math.floor(Math.random()*rpsArray.length))];
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection) {
+        return "Draw";
+    }
+    else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Paper" && computerSelection === "Rock") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        return "Player Wins";
+    }
+    else {
+        return "Computer Wins";
+    }
+}
+
+console.log("Player chose " + playerSelection);
+console.log("Computer chose " + computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
