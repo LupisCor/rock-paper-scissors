e62b5e91350dc1d8ac2ed5acfc983cf484cd411c
Lupis_ <doliver575@gmail.com>
2023-08-08 10:31:03 -0400

    Implement while loop to play 5 rounds of rps. All outcomes clearly stated

--- javascript.js
+++ javascript.js
@@ -2,28 +2,57 @@ console.log("Hello World");
 
 let rpsArray = ["Rock", "Paper", "Scissors"]
 
+
+let roundsPlayed = 0;
+let playerScore = 0;
+let computerScore = 0;
+let drawScore = 0;
+
+while (roundsPlayed < 5) {
+    playerSelection = prompt("Please select 'Rock', 'Paper', or 'Scissors'.");
+    computerSelection = getComputerChoice();
+    console.log("Player chose " + playerSelection);
+    console.log("Computer chose " + computerSelection)
+    console.log(playRound(playerSelection, computerSelection));
+
+}
+
+if (playerScore > computerScore){
+    console.log("Final Score: Player Wins: " + playerScore + ", Computer Wins: " + computerScore + ", Draws: " + drawScore);
+    console.log("Player Wins The Game!");
+}
+else if (playerScore < computerScore){
+    console.log("Final Score: Player Wins: " + playerScore + ", Computer Wins: " + computerScore + ", Draws: " + drawScore);
+    console.log("Computer Wins The Game!");
+}
+else{
+    console.log("Final Score: Player Wins: " + playerScore + ", Computer Wins: " + computerScore + ", Draws: " + drawScore);
+    console.log("Tie Game");
+}
+
 function getComputerChoice(){
     return rpsArray[(Math.floor(Math.random()*rpsArray.length))];
 }
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-
 function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
+        drawScore++;
+        roundsPlayed++;
         return "Draw";
     }
-    else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
-        (playerSelection === "Paper" && computerSelection === "Rock") ||
-        (playerSelection === "Scissors" && computerSelection === "Paper")) {
-        return "Player Wins";
+    else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")) {
+        playerScore++;
+        roundsPlayed++;
+        return "Player Wins round " + roundsPlayed;
     }
     else {
-        return "Computer Wins";
+        computerScore++;
+        roundsPlayed++
+        return "Computer Wins round " + roundsPlayed;
     }
 }
 
-console.log("Player chose " + playerSelection);
-console.log("Computer chose " + computerSelection);
-
-console.log(playRound(playerSelection, computerSelection));
